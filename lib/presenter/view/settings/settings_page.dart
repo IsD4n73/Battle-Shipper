5eac4161c8742772ef63d04553726c66642e7aaa
dante.davidde <dante.davidde@accenture.com>
2023-11-21 17:53:27 +0100

    fix file tree

--- lib/presenter/view/settings/settings_page.dart
+++ lib/presenter/view/settings/settings_page.dart
@@ -1,5 +1,5 @@
-import 'package:battle_shipper/view/settings/widget/audiobg_widget.dart';
-import '../../../view/settings/widget/language_widget.dart';
+import 'package:battle_shipper/presenter/view/settings/widget/audiobg_widget.dart';
+import 'package:battle_shipper/presenter/view/settings/widget/language_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
