c88090a39d685dcba6c15ebf34e124d66897a44f
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-12-11 21:37:50 +0100

    Update and rename CommunicationModel.dart to communication_model.dart

--- lib/domain/entities/communication_model.dart
+++ lib/domain/entities/communication_model.dart
@@ -3,12 +3,14 @@ class CommunicationModel {
   String value;
   String peerId;
   String message;
+  String username;
 
   CommunicationModel({
     required this.command,
     required this.value,
     required this.peerId,
     required this.message,
+    required this.username,
   });
 
   factory CommunicationModel.fromJson(Map<String, dynamic> json) =>
@@ -17,10 +19,11 @@ class CommunicationModel {
         value: json["value"],
         peerId: json["peerId"],
         message: json["message"],
+        username: json["username"], 
       );
 
   @override
   String toString() {
-    return '{"command": "$command", "value": "$value", "peerId": "$peerId", "message": "$message"';
+    return '{"command": "$command", "value": "$value", "peerId": "$peerId", "message": "$message"', "username": "$username";
   }
 }
